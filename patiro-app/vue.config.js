bcc94022061fe412ae31e8dd178d8618f69376b8
Iryna-Grabovska <mrsirynagrabovska@gmail.com>
2022-05-28 14:19:23 +0200

    made app

--- patiro-app/vue.config.js
+++ patiro-app/vue.config.js
@@ -1,4 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+});
+
+// module.exports = {
+//   publicPath: ["/patiro-app"],
+// };
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/patiro-app/" : "/",
+};
